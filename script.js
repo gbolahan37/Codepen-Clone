5b48b812e8d4c70bd00c715ab88376f09bf9895a
“Gbolahan <gbolahansalvador37@gmail.com>
2025-10-22 15:16:58 +0100

    hhfh

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
 let sidebarToggle = document.querySelectorAll(".sidebar-toggle")
 let sidebarDetails = document.querySelectorAll(".sidebar-details")
-let sidebarImage = document.querySelector("#side-img")
+let sidebarImage = document.querySelectorAll(".side-img")
 
-let sideImages = [
-    {
-        image: "img/Screenshot 2025-10-20 142639.png",
-    },
-    {
-        image: "img/Screenshot 2025-10-19 212358.png",
-    },
-    {
-        image: "img/Screenshot 2025-10-20 140918.png",
-    },
-    {
-        image: "img/Screenshot 2025-10-20 140932.png",
-    },
-    {
-        image: "img/Screenshot 2025-10-20 140947.png",
-    },
-    {
-        image: "img/Screenshot 2025-10-20 141010.png",
-    },
-]
 
 function displayContent (index){
-    sidebarDetails.forEach(detail => detail.classList.remove("active"));
-    sidebarDetails[index].classList.add("active")
-    sidebarImage.setAttribute("src", sideImages[index].image) 
+    sidebarDetails.forEach(detail => detail.classList.remove("active"))
+    sidebarDetails[index].classList.add("active") 
+    sidebarImage.forEach(asset => asset.classList.remove("move"))
+    sidebarImage[index].classList.add("move")
     
 }
 
